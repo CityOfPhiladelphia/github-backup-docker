cada039a1d3375ba5a8c4d40e71b8e8249548001
Dan Lopez <dan.lopez@phila.gov>
2019-11-27 11:43:16 -0500

    Minor updates

--- backup.sh
+++ backup.sh
@@ -6,6 +6,10 @@ slack_failure () {
   curl -X POST -H 'Content-type: application/json' --data '{"text":"Github Backup Failed"}' $SLACK_WEBHOOK_URL
 }
 
+slack_success () {
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"Github Backup Complete"}' $SLACK_WEBHOOK_URL
+}
+
 echo "backing up"
 github-backup $GITHUB_ORGANIZATION --token $GITHUB_ACCESS_TOKEN --organization -o . --repositories -i
 echo "done backing up"
@@ -15,4 +19,4 @@ echo "syncing to s3"
 aws s3 sync . s3://${BACKUP_S3_BUCKET} --region us-east-1
 echo "s3 sync complete"
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Github Backup Complete"}' $SLACK_WEBHOOK_URL
+slack_success
